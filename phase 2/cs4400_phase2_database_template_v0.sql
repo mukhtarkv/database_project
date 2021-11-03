060bb9dd6eef049d4cec3026bfee87bf3ee645bd
Mukhtar Kussaiynbekov <mukhtarkussaiynbekov@gmail.com>
2021-11-03 12:46:35 -0400

    initialize repo for group project

---
+++ phase 2/cs4400_phase2_database_template_v0.sql
@@ -0,0 +1,6 @@
+-- CS4400: Introduction to Database Systems (Fall 2021)
+-- Phase II: Create Table & Insert Statements [v0] Thursday, October 14, 2021 @ 2:00pm EDT
+
+-- ------------------------------------------------------
+-- CREATE TABLE STATEMENTS AND INSERT STATEMENTS BELOW
+-- ------------------------------------------------------
